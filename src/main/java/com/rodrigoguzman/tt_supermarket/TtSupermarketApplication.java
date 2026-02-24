4dfd81b4424e8f7bfb05ccf372ffaba07667121c
Rodrigo Guzman <rodrigoguzman6796@gmail.com>
2026-02-24 14:00:48 -0300

    Create project structure

---
+++ src/main/java/com/rodrigoguzman/tt_supermarket/TtSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoguzman.tt_supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigoguzman/tt_supermarket/TtSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoguzman.tt_supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
